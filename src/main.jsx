393e637d3d5eeff40468233175c4a1ae9e187e7d
Mohamad <m.yousefi.isf@gmail.com>
2024-10-15 12:04:20 +0330

    setup .env file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
+// console.log()
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
 );
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register(`http://localhost:3007/firebase-messaging-sw.js`)
+    .register(`${import.meta.env.VITE_ROOT}/firebase-messaging-sw.js`)
     .then((registration) => {
       console.log(
         "Service Worker registration successful with scope: ",
